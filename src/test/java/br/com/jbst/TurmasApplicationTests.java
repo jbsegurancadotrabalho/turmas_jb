9fddc19c0bc8c5eeb2c6cb065b4a34e381e95bdc
Diego Bizerra da Silva <diegorj93@gmail.com>
2024-07-24 17:50:54 -0300

    Foi corrigido o mapeamento no arquivo application.

--- src/test/java/br/com/jbst/TurmasApplicationTests.java
+++ src/test/java/br/com/jbst/TurmasApplicationTests.java
@@ -1,13 +1,13 @@
-package br.com.jbst;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class TurmasApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package br.com.jbst;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class TurmasApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
